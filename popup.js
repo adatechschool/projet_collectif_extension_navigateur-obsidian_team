94e801d9ea588291b18a87dc4e4a36f601204967
Sandy CS <89575609+Shaa94@users.noreply.github.com>
2022-05-03 17:07:02 +0200

    Feat glissière timer + champs texte - HTML/CSS/JS

--- popup.js
+++ popup.js
@@ -1,20 +1,29 @@
-let changeColor = document.getElementById("changeColor");
+// Get the modal
+var modal = document.getElementById("myModal");
 
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
-});
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
 
-changeColor.addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      function: setPageBackgroundColor,
-    });
-  });
-  
-  function setPageBackgroundColor() {
-    chrome.storage.sync.get("color", ({ color }) => {
-      document.body.style.backgroundColor = color;
-    });
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+function updateTextInput(val) {
+    document.getElementById('text-input').value=val; 
   }
