42966ac3480e9dc10492c1b8e8c15a0d9eebf071
Bynood <quent1_jarry@hotmail.com>
2022-05-06 17:14:11 +0200

    v1 event handling front -> background

--- background.js
+++ background.js
@@ -1,32 +1,3 @@
-// Set the date we're counting down to
-var countDownDate = new Date("Jan 5, 2024 15:37:25").getTime();
-
-// Update the count down every 1 second
-var x = setInterval(function() {
-
-  // Get today's date and time
-  var now = new Date().getTime();
-
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
-
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  // Display the result in the element with id="demo"
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
-
-  // If the count down is finished, write some text
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
-  }
-}, 1000);
-
 // Timer code
 class Timer {
   constructor(callback) {
@@ -65,6 +36,15 @@ class Timer {
   }
 }
 
+//Create timer instance
 const timer = new Timer(() => {
   console.log("countdown is over!");
 })
+
+//Receive events from popup
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse){
+    console.log(`request : ${request.event}`)
+    sendResponse({status: `background received request for ${request.event}`})
+  }
+);
