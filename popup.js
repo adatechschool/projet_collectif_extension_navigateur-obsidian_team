492ad8736bde22313403d880de4e0d21dc812bb2
Sexy-Red-Wolf <98813918+Sexy-Red-Wolf@users.noreply.github.com>
2022-05-09 16:04:08 +0200

    handling the pause, stop and play buttons + fixing some bugs

--- popup.js
+++ popup.js
@@ -47,6 +47,7 @@ setInterval(function () {
 // Event functions
 function playEvent() {
   console.log ("play pressed")
+  }
   /*Check current status of timer 
     Event triggers if timer on pause or stopped
     Event triggers only if textInput value is a valid format (int)
@@ -58,22 +59,33 @@ function playEvent() {
       console.log(response.status)
     })  
   } else {
+    console.log("incorrect input, please enter a valid number")
     //Message indicating the input value is not correct
   }
 }
 
 function pauseEvent() {
+  const state = chrome.storage.sync.get({ state });
+  let remainingTime = timerValue
+
+  if (state == "isPaused"){
+    chrome.runtime.sendMessage({event: "pause"}, {time: remainingTime}, function(response){
+    console.log(response.status)
   /*Check current status of timer 
     Event triggers if timer playing
   */
   console.log ("pause pressed")
 }
-
-function stopEvent() {
+  }
+}
+function stopEvent(){
+  const state = chrome.storage.sync.get({ state });
+  if (state == "isActive" || state == "isPaused"){
+    chrome.runtime.sendMessage({event: "stop"}, function(response){
+    console.log(response.status)
+  }
+}
+}
   /*Check current status of timer 
     Event does not trigger if timer already stopped
   */
-  console.log ("stop pressed")
-}
-
-
