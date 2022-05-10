07e8dcc0d7fb00b2768e68072342fa514033a830
Bynood <quent1_jarry@hotmail.com>
2022-05-10 16:19:49 +0200

    feat on startup popup call timer

--- popup.js
+++ popup.js
@@ -31,6 +31,12 @@ pause.addEventListener("click", pauseEvent);
 let stoop = document.getElementById("stoop");
 stoop.addEventListener("click", stopEvent);
 
+// From Storage API, get current timer status
+chrome.runtime.onStartup.addListener(() =>{
+  chrome.storage.sync.get ({ timer })
+  console.log(`Success import for timer with values ${timer.state} ${timer.remainingTime}`)
+});
+
 
 // Countdown logic
 let countdownNumberEl = document.getElementById('countdown-number');
